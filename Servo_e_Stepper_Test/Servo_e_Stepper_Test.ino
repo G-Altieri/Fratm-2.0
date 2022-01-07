e2b1d46be63b5393506eb9b7c316507795dd1d5b
Giovanni Altieri <79712043+G-Altieri@users.noreply.github.com>
2022-01-07 03:26:42 +0100

    Fratm 2.0 base and first arm
    Software movement with limit

--- Servo_e_Stepper_Test/Servo_e_Stepper_Test.ino
+++ Servo_e_Stepper_Test/Servo_e_Stepper_Test.ino
@@ -16,6 +16,8 @@ int motorPin2 = 50;
 int motorPin3 = 49;
 int motorPin4 = 48;
 int motor_Speed = 2;
+int stepX = 200;
+
 Stepper myStepper(stepsPerRevolution, motorPin4, motorPin2, motorPin3, motorPin1);
 
 int button_1 = 12; //Pulsante 1 sul pin 2
@@ -26,14 +28,17 @@ int button_2 = 11; //Pulsante 2 sul pin 3
 int val;
 int val1 = 0;
 int val2 = 0;
-int valStepper = 250;
+
+//Stepper Var
+int valStepper = 0;
+int SteperLimit = 1800;
 
 
 //Var Servox
-int ServoxVelocity = 15;
+int ServoxVelocity = 30;
 int ServoxPos = 90;
-int ServoxConst = 5;
-int ServoxLimitS = 130;
+int ServoxConst = 15;
+int ServoxLimitS = 140;
 int ServoxLimitD = 40;
 
 void setup() {
@@ -56,7 +61,7 @@ void setup() {
 
 
   //Timer set
-  t1.set(5000);
+  // t1.set(5000);
 
   Serial.println("Arduino is ready");
 }//setup
@@ -64,40 +69,11 @@ void setup() {
 void loop() {
 
   //Timer
-  /*if (t1.check()) {
-     Serial.println("Timer 1");
-     t1.set(5000);
-     moveTo(100,10);
-    }*/
-
-  val1 = digitalRead(button_1);
-  if (val1 == HIGH) {
-    Serial.println("Senso Orario");
-    // myStepper.step(stepsPerRevolution);
-    // delay(500);
-    // myStepper.step(valStepper);
-    moveTo(100, 10);
-    delay(500);
+  if (t1.check()) {
+    Serial.println("Timer 1");
+    // t1.set(5000);
+    moveTo(130, 10);
   }
-
-  val2 = digitalRead(button_2);  //Quando premo il pulsante 2 gira a destra
-  if (val2 == HIGH) {
-    Serial.println("Senso AntiOrario");
-    // myStepper.step(-stepsPerRevolution);
-    // delay(500);
-    // myStepper.step(-valStepper);
-    moveTo(60, 20);
-    delay(500);
-  }
-
-  /*
-    //Servo with potenziometro
-    val = analogRead(potentiometerPin);
-    val = map(val, 0, 1023, 45, 127);
-    Serial.println(val);
-    Servox.write(val);
-    delay(300);
-  */
 
   char x;
 
@@ -109,69 +85,81 @@ void loop() {
     Serial.println(x);
 
     switch (x) {
-      case '1':
-
-        Serial.print("ServoxVelocity ");
-        if (ServoxVelocity < 30) {
-          ServoxVelocity = ServoxVelocity + 1;
-        }
-        Serial.println(ServoxVelocity);
-        break;
-      case '2':
-        if (ServoxVelocity > 2) {
-          ServoxVelocity = ServoxVelocity - 1;
-        }
-        Serial.print("ServoxVelocity ");
-        Serial.println(ServoxVelocity);
-
-        break;
-      case '3':
-        ServoxConst = ServoxConst + 1;
-        Serial.print("ServoxConst ");
-        Serial.println(ServoxConst);
-        break;
-      case '4':
-        ServoxConst = ServoxConst - 1;
-        Serial.print("ServoxConst ");
-        Serial.println(ServoxConst);
-        break;
+      /*   case '1':
+           Serial.print("ServoxVelocity ");
+           if (ServoxVelocity < 30) {
+             ServoxVelocity = ServoxVelocity + 1;
+           }
+           Serial.println(ServoxVelocity);
+           break;
+         case '2':
+           if (ServoxVelocity > 2) {
+             ServoxVelocity = ServoxVelocity - 1;
+           }
+           Serial.print("ServoxVelocity ");
+           Serial.println(ServoxVelocity);4r4rrwddddwdwdwdwrwwwwwwwawdw
+           break;
+         case '3':
+           ServoxConst = ServoxConst + 1;
+           Serial.print("ServoxConst ");
+           Serial.println(ServoxConst);
+           break;
+         case '4':
+           ServoxConst = ServoxConst - 1;
+           Serial.print("ServoxConst ");
+           Serial.println(ServoxConst);
+           break;
+      */
+      /*Stepper Control*/
       case 'a':
-        moveTo(120, 10);
-        break;
-      case 'w':
-        moveTo(ServoxPos + ServoxConst, ServoxVelocity);
-        Serial.print("New Position Servo ");
-        Serial.println(ServoxPos + ServoxConst);
-        if (ServoxPos >= ServoxLimitD) {
-          ServoxPos = ServoxPos - ServoxConst;
+        Serial.print("Giro in senso Orario");
+        if (valStepper < SteperLimit) {
+          myStepper.step(stepX);
+          valStepper = valStepper + stepX;
+          Serial.println(valStepper);
         }
+
+
         break;
       case 'd':
-        moveTo(120, 10);
+        Serial.print("Giro in senso Antiorario");
+        if (valStepper > -SteperLimit) {
+          myStepper.step(-stepX);
+          valStepper = valStepper - stepX;
+          Serial.println(valStepper);
+        }
+
         break;
-      case 's':
-        moveTo(ServoxPos + ServoxConst, ServoxVelocity);
-        Serial.print("New Position Servo ");
-        Serial.println(ServoxPos + ServoxConst);
-        if (ServoxPos <= ServoxLimitS) {
+
+      /*Servo Control*/
+      case 'w':
+        if ((ServoxPos + ServoxConst) <= ServoxLimitS) {
           ServoxPos = ServoxPos + ServoxConst;
+          moveTo(ServoxPos, ServoxVelocity);
+          Serial.print("New Position Servo ");
+          Serial.println(ServoxPos);
         }
         break;
-      case 'z':
-        myStepper.step(1000);
+      case 's':
+        if ((ServoxPos - ServoxConst) >= ServoxLimitD) {
+          ServoxPos = ServoxPos - ServoxConst;
+          moveTo(ServoxPos, ServoxVelocity);
+          Serial.print("New Position Servo ");
+          Serial.println(ServoxPos);
+        }
         break;
-      case 'x':
-        myStepper.step(-1024);
-        break;
-      case 'c':
-        myStepper.step(1000);
-        break;
-      case 'v':
-        myStepper.step(1002);
-        break;
-      case 'g':
-        prendiChiavi();
-        break;
+      /*
+            case 'z':
+              Serial.print("Step incrementati");
+              Serial.println(stepX);
+              stepX = stepX + 50;
+              break;
+            case 'x':
+              Serial.print("Step decrementati");
+              Serial.println(stepX);
+              stepX = stepX - 50;
+              break;
+      */
       case 'r':
         reset();
         break;
@@ -196,17 +184,17 @@ void prendiChiavi() {
 }
 
 void reset() {
+  ServoxPos = 90;
   Serial.print("Reset Servox ");
-  Serial.println(90);
-  moveTo(90, 30);
+  Serial.println(ServoxPos);
+  moveTo(ServoxPos, 30);
 }
 
-
+/*Method for move Servo*/
 int pos1 = 90;
-int mapSpeed ;
+int mapSpeed;
 int pos;
 void moveTo(int position, int speed) {
-
   mapSpeed = map(speed, 0, 30, 30, 0);
   if (position > pos) {
     for (pos = pos1; pos <= position; pos += 1) {
@@ -224,3 +212,34 @@ void moveTo(int position, int speed) {
 
 
 }
+
+
+/*
+  val1 = digitalRead(button_1);
+  if (val1 == HIGH) {
+    Serial.println("Senso Orario");
+    // myStepper.step(stepsPerRevolution);
+    // delay(500);
+    // myStepper.step(valStepper);
+    moveTo(100, 10);
+    delay(500);
+  }
+
+  val2 = digitalRead(button_2);  //Quando premo il pulsante 2 gira a destra
+  if (val2 == HIGH) {
+    Serial.println("Senso AntiOrario");
+    // myStepper.step(-stepsPerRevolution);
+    // delay(500);
+    // myStepper.step(-valStepper);
+    moveTo(60, 20);
+    delay(500);
+  }
+
+
+    //Servo with potenziometro
+    val = analogRead(potentiometerPin);
+    val = map(val, 0, 1023, 45, 127);
+    Serial.println(val);
+    Servox.write(val);
+    delay(300);
+*/
